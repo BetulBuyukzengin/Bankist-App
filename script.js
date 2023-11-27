f7f140150718642f7089853295b9ecbaa4fedc9e
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2023-11-28 01:23:10 +0300

    timer ve date özelliklerini ekledim

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2023-11-22T13:15:33.035Z',
+    '2023-11-23T09:48:16.867Z',
+    '2023-12-24T06:04:23.907Z',
+    '2023-01-24T14:18:46.235Z',
+    '2023-02-25T16:33:06.386Z',
+    '2023-04-26T14:43:26.374Z',
+    '2023-06-26T18:49:59.371Z',
+    '2023-07-27T12:01:20.894Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -14,13 +27,39 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2023-11-22T13:15:33.035Z',
+    '2023-11-23T09:48:16.867Z',
+    '2023-12-24T06:04:23.907Z',
+    '2023-01-24T14:18:46.235Z',
+    '2023-02-25T16:33:06.386Z',
+    '2023-04-26T14:43:26.374Z',
+    '2023-06-26T18:49:59.371Z',
+    '2023-07-27T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
-  owner: 'Mert Altın ',
+  owner: 'Mert Altan',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+
+  movementsDates: [
+    '2023-11-22T13:15:33.035Z',
+    '2023-11-23T09:48:16.867Z',
+    '2023-12-24T06:04:23.907Z',
+    '2023-01-24T14:18:46.235Z',
+    '2023-02-25T16:33:06.386Z',
+    '2023-04-26T14:43:26.374Z',
+    '2023-06-26T18:49:59.371Z',
+    '2023-07-27T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account4 = {
@@ -28,6 +67,18 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  movementsDates: [
+    '2023-11-22T13:15:33.035Z',
+    '2023-11-23T09:48:16.867Z',
+    '2023-12-24T06:04:23.907Z',
+    '2023-01-24T14:18:46.235Z',
+    '2023-02-25T16:33:06.386Z',
+    '2023-04-26T14:43:26.374Z',
+    '2023-06-26T18:49:59.371Z',
+    '2023-07-27T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -57,23 +108,105 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
-//sort parametresi eklendi
-const displayMovements = function (movement, sort = false) {
+
+// Functions
+
+const formatMovementDate = function (date, locale) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 24 * 60 * 60));
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  //*get Api
+  return new Intl.DateTimeFormat(locale).format(date);
+  /*  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
+  //day/month/year */
+
+  /*  
+ return çalıştığı an fonksiyondan çıktığı için else e gerek yok, if leri  sağlamadığı an 
+ zaten alttakı kodları çalıştıracak 
+
+ else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+    //day/month/year
+  } */
+};
+//para birimleri için fonk API
+const formatCur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+  }).format(value);
+};
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
-  //sort
-  const movs = sort ? movement.slice().sort((a, b) => a - b) : movement;
+
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
+
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
-    const html = ` <div class="movements__row">
-    <div class="movements__type movements__type--${type}">${i + 1}${type}</div>
-    <div class="movements__value">${mov}€</div>
-  </div>`;
+    const date = new Date(acc.movementsDates[i]);
+    const displayDate = formatMovementDate(date, acc.locale);
+
+    const formattedMov = formatCur(mov, acc.locale, acc.currency);
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+    <div class="movements__date">${displayDate}</div>
+
+        <div class="movements__value">${formattedMov} </div>
+      </div>
+    `;
+
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 
-//createUsernames
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
+};
+
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
+
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = formatCur(out, acc.locale, acc.currency);
+
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      // console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);
+};
+
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
@@ -84,161 +217,193 @@ const createUsernames = function (accs) {
   });
 };
 createUsernames(accounts);
-console.log(accounts);
 
 const updateUI = function (acc) {
-  //display movements
-  displayMovements(acc.movements);
+  // Display movements
+  displayMovements(acc);
 
-  //display balance
+  // Display balance
   calcDisplayBalance(acc);
 
-  //display summary
+  // Display summary
   calcDisplaySummary(acc);
 };
-const calcDisplayBalance = function (acc) {
-  acc.balance = acc.movements.reduce((acc, cur) => acc + cur, 0);
-  labelBalance.textContent = `${acc.balance} €`;
+
+const startLogOutTimer = function () {
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+    //in each call, print the remaining time to UI
+    labelTimer.textContent = `${min}:${sec}`;
+
+    //when 0 seconds,stop timer and log out user
+    if (time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = 'Log in to get started';
+      containerApp.style.opacity = 0;
+    }
+    //decrese is
+    time--;
+  };
+  //set time 5 minutes
+  let time = 120;
+
+  //call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+  return timer;
 };
+///////////////////////////////////////
+// Event handlers
+let currentAccount, timer;
 
-//incomes
-const calcDisplaySummary = function (acc) {
-  const incomes = acc.movements
-    .filter(mov => mov > 0)
-    .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
-
-  //expenses
-  const expenses = acc.movements
-    .filter(mov => mov < 0)
-    .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(expenses)}€`;
-
-  const interest = acc.movements
-    .filter(mov => mov > 0)
-    .map(deposit => (deposit * acc.interestRate) / 100)
-    .filter((int, i, arr) => {
-      console.log(arr);
-      return int >= 1;
-    })
-    .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
-};
-
-// EVENT HANDLERS
-let currentAccount;
+/* //* fake always logged in
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 100; */
 
 btnLogin.addEventListener('click', function (e) {
+  // Prevent form from submitting
   e.preventDefault();
+
   currentAccount = accounts.find(
     acc => acc.username === inputLoginUsername.value
   );
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
-    //display UI and welcome message
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === +inputLoginPin.value) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    /* API siz tarih oluşturma
+     //create current date and time
+    const now = new Date();
+    const day = `${now.getDate()}`.padStart(2, 0);
+    const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    const year = now.getFullYear();
+    const hour = `${now.getHours()}`.padStart(2, 0);
+    const minute = `${now.getMinutes()}`.padStart(2, 0);
+        labelDate.textContent = `${day}/${month}/${year}-${hour}:${minute}`;
+s
+ */
+
+    //* Experimenting API ---Tarih çekme
+    const now = new Date();
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      month: 'numeric',
+      //year:'2-digit',
+      year: 'numeric',
+      //weekday: 'long',
+    };
+    /*  //güncel lokasyonu alır 
+    const locale = navigator.language;
+    */
+    //lokasyona göre format
+    //navigator.geolocation();
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now);
+
+    // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
-    containerApp.style.opacity = 100;
-    labelWelcome.textContent = `WELCOME back,${currentAccount.owner}`;
+
+    //timer
+    if (timer) clearInterval(timer);
+    timer = startLogOutTimer();
+    
+    // Update UI
     updateUI(currentAccount);
-  } else {
-    alert('There is not user like that');
-    labelWelcome.textContent = 'Login to get started';
-    containerApp.style.opacity = 0;
   }
 });
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
-  console.log(amount, receiverAcc);
-  inputTransferTo.value = inputTransferAmount.value = '';
+  inputTransferAmount.value = inputTransferTo.value = '';
 
   if (
     amount > 0 &&
     receiverAcc &&
     currentAccount.balance >= amount &&
-    receiverAcc.username !== currentAccount.username
+    receiverAcc?.username !== currentAccount.username
   ) {
-    //doing the transfer
-    currentAccount.movements.push(-amount); //current acc
-    receiverAcc.movements.push(amount); //receiver acc
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+    //Add transfer date
+    currentAccount.movementsDates.push(new Date().toISOString());
+    //alıcı hareket tarihi
+    receiverAcc.movementsDates.push(new Date().toISOString());
+    // Update UI
     updateUI(currentAccount);
+
+    //reset timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
+
   }
 });
 
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputLoanAmount.value);
+
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    //add movement
-    currentAccount.movements.push(amount);
-    //update UI
-    updateUI(currentAccount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
+      //Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
+
+      // Update UI
+      updateUI(currentAccount);
+
+      //reset timer
+      clearInterval(timer);
+      timer = startLogOutTimer();
+    }, 2500);
   }
   inputLoanAmount.value = '';
-  inputLoanAmount.blur();
 });
 
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
+
   if (
-    Number(inputClosePin.value) === currentAccount.pin &&
-    inputCloseUsername.value === currentAccount.username
+    inputCloseUsername.value === currentAccount.username &&
+    +inputClosePin.value === currentAccount.pin
   ) {
-    const deleteIndex = accounts.findIndex(
+    const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
     );
-    //delete account
-    accounts.splice(deleteIndex, 1);
-    //hide UI
+    console.log(index);
+    // .indexOf(23)
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
     containerApp.style.opacity = 0;
-    alert('USER DELETED');
   }
-  inputClosePin.value = inputCloseUsername.value = '';
+
+  inputCloseUsername.value = inputClosePin.value = '';
 });
+
 let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
-  displayMovements(currentAccount.movements, !sorted);
+  displayMovements(currentAccount, !sorted);
   sorted = !sorted;
 });
-
-
-//depositeleri alıp euro dan dolara çevirme
-
-/* const totalDepositsUSD = movements
-  .filter(mov => mov > 0)
-  .map(mov => mov * eurToUsd)
-  .reduce((acc, mov) => acc + mov, 0);
-console.log(totalDepositsUSD);
-
-const totalWithdrawal = Math.abs(
-  movements
-    .filter(mov => mov < 0)
-    .map(mov => mov * eurToUsd)
-    .reduce((acc, mov) => acc + mov, 0)
-);
-
-console.log(totalWithdrawal); // çekilen dolar miktarı */
-
-
-
-labelBalance.addEventListener('click', function () {
-  //Array.from()
-  const movementsUI = Array.from(
-    document.querySelectorAll('.movements__value'),
-    el => Number(el.textContent.replace('€', ''))
-  );
-  console.log(movementsUI);
-  //Spread Operator
-  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
-  const mappedArray = movementsUI2.map(a =>
-    Number(a.textContent.replace('€', ''))
-  );
-  console.log(mappedArray);
-});
-
